f349535c4004081ce8137722b2e37eed7fa3b042
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-22 12:23:12 -0700

    renderLicenseSection function created

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -32,7 +32,23 @@ function renderLicenseLink(license) {
 }
 
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'The MIT License') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    THIS APPLICATION  IS COVERED UNDER THE MIT LICENSE`
+
+  } else if (license === 'Apache 2.0 License') {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    THIS APPLICATION  IS COVERED UNDER Apache 2.0 License`
+
+  } else if (license === 'GNU GPL v3') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+    THIS APPLICATION  IS COVERED UNDER GNU GPL v3`
+
+  } else {
+    return '';
+  }
+}
 
 function generateMarkdown(data) {
   return `# ${data.title}
